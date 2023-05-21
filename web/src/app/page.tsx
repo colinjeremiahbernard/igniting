875864a84e4a0bdf42170efd8d88ea340165ac20
Colin Stephenson <colinjeremiahbernard@gmail.com>
2023-05-21 16:30:48 -0300

    image, layout and page tsx, tailwindcss.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,9 +4,21 @@
 
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua capsula do tempo!</h1>
-    </div>
+    <main className="grid grid-cols-2 min-h-screen">
+      {/*Left*/}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+        <div className="absolute left-0 top-1/2 h-[288px] w-[526px] bg-pink-500 opacity-50 -translate-y-1/2 -translate-x-1/2 rounded-full blur-full"  />
+        <div className="absolute right-0 top-0 bottom-0 w-2 bg-stripes"></div>
+      </div>
+      {/*Right*/}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+            <p className="w-[360px] text-center leading-relaxed">No registered past memory, start{''} <a href="" className="underline hover:text-brown-50">now</a>!
+            </p>
+        </div>
+      </div>
+    </main>
+    
     
   )
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,10 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans:'var(--font-roboto)',
+        alt:'var(--font-bai-jamjuree)',
+      },
+      colors:{
+      },
+      backgroundImage:{
+        stripes:'linear-gradient(to bottom, rgba(255,255,255,0.1),rgba(255,255,255,0.1)12.5%, transparent 12.5%, transparent)',
+      },
+      backgroundSize:{
+        stripes:'100% 8px',
+      }, 
+      blur:{
+        full:'194px'
       },
     },
   },
